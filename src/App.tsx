b93123f00ad05566dd6c7470f838226fa337946f
gayeongX0916 <gayeongx0916@gmail.com>
2025-06-09 20:22:27 +0900

    [feature] useReducer 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,76 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import "./App.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 import type { TodosProps } from "./types/TodosProps";
 
+type Action =
+  | { type: "Add"; payload: string }
+  | { type: "Edit"; payload: { id: number; value: string } }
+  | { type: "Delete"; payload: number }
+  | { type: "Toggle"; payload: number };
+
+function reducer(state: TodosProps[], action: Action): TodosProps[] {
+  switch (action.type) {
+    case "Add":
+      return [...state, { id: Date.now(), done: false, text: action.payload }];
+    case "Edit":
+      return state.map((todo) =>
+        todo.id === action.payload.id
+          ? { ...todo, text: action.payload.value }
+          : todo
+      );
+    case "Delete":
+      return state.filter((todo) => todo.id !== action.payload);
+    case "Toggle":
+      return state.map((todo) =>
+        todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+      );
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState<TodosProps[]>(() => {
+  const [state, dispatch] = useReducer(reducer, [], () => {
     const saved = localStorage.getItem("todos");
     return saved ? JSON.parse(saved) : [];
   });
-
   const handleAddTodo = (value: string) => {
-    const newAdd = {
-      done: false,
-      text: value,
-      id: Date.now(),
-    };
-    setTodos((prev) => [...prev, newAdd]);
+    dispatch({
+      type: "Add",
+      payload: value,
+    });
   };
 
   const handleEditTodo = (id: number, value: string) => {
-    setTodos((prev) =>
-      prev.map((todo) => (todo.id === id ? { ...todo, text: value } : todo))
-    );
+    dispatch({
+      type: "Edit",
+      payload: { id, value },
+    });
   };
 
   const handleDeleteTodo = (id: number) => {
-    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+    dispatch({
+      type: "Delete",
+      payload: id,
+    });
   };
 
   const handleToggle = (id: number) => {
-    setTodos((prev) =>
-      prev.map((todo) =>
-        todo.id === id ? { ...todo, done: !todo.done } : todo
-      )
-    );
+    dispatch({
+      type: "Toggle",
+      payload: id,
+    });
   };
 
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
+    localStorage.setItem("todos", JSON.stringify(state));
+  }, [state]);
 
   return (
     <div className="todo-list">
       <TodoInput onAdd={handleAddTodo} />
       <TodoList
-        todos={todos}
+        todos={state}
         onEdit={handleEditTodo}
         onDelete={handleDeleteTodo}
         onToggle={handleToggle}
